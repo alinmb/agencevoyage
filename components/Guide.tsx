29fc7ebb62af7f50a9d4e04da527f68706c958b3
Ali Nmb <n.ali75012@gmail.com>
2024-01-01 15:12:33 +0100

    Feat: GetApp ok + responsive

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -15,7 +15,7 @@ const Guide = () => {
         <p className="uppercase regular-18 -mt-1 mb-3 text-green-50"></p>
         <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
           <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">
-            Vous guider vers un chemin facile
+            Vers un chemin facile
           </h2>
           <p className="regular-16 text-gray-30 xl:max-w-[520px]">
             Ce n'est qu'avec l'application Hilink que vous ne vous perdrez plus.
